e08c02209cb09e3feafff14b692be0013a3dc937
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-08-06 00:11:55 +0100

    Update station.rb

--- lib/station.rb
+++ lib/station.rb
@@ -2,9 +2,9 @@ require './lib/station_terminal_out.rb'
 require './lib/station_terminal_in.rb'
 require './lib/user_terminal.rb'
 class Station
-
   attr_reader :name, :zone, :terminal_in, :terminal_out
   attr_reader :user_terminal
+
   def initialize(name = ("a".."g").to_a.shuffle.join.capitalize, zone = rand(1..5))
     @name = name
     @zone = zone

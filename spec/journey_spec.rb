bea6114da918acd571793424fbc10609ae9c377e
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-08-06 12:52:14 +0100

    Delete journey_spec.rb

--- spec/journey_spec.rb
+++
@@ -1,7 +0,0 @@
-require "./lib/journey.rb"
-describe Journey do
-  it "journey has an attribute content_view[:from_station] equal to Paddington" do
-    subject = described_class.new("Paddington", 1)
-    expect(subject.content_view[:from_station]).to eq("Paddington")
-  end
-end

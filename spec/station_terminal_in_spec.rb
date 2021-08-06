03acb142ea0258a5a4b3977ad7b98ad739b91a61
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-08-06 12:53:07 +0100

    Delete station_terminal_in_spec.rb

--- spec/station_terminal_in_spec.rb
+++
@@ -1,8 +0,0 @@
-require "./lib/station_terminal_in.rb"
-
-describe Station_Terminal_In do
-  subject(:terminal_in){ described_class.new("Paddington", 1) }
-  it "touch is a method of Station_Terminal_In" do
-    expect(terminal_in).to respond_to(:touch)
-  end
-end

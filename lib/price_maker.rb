2e0501e9269b5c0847cb561c350bc0734a9a1240
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-08-06 12:50:52 +0100

    Delete price_maker.rb

--- lib/price_maker.rb
+++
@@ -1,9 +0,0 @@
-class Price_Maker
-
-  def set_value(journey, to_station, to_zone, to_time = Time.now.to_i)
-    journey = journey.content_view
-    @from_zone, @from_time, @from_station = journey[:from_zone], journey[:from_time], journey[:from_station]
-    return 300 # until we get set value how to..xwwxw
-  end
-
-end

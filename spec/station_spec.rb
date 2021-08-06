9576921882de36bc264dab89d3a1520feb697637
Radu Necsanu <71831322+necsanuradu@users.noreply.github.com>
2021-08-06 12:52:57 +0100

    Delete station_spec.rb

--- spec/station_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'station'
-
-describe Station do
-  it 'should have a name and a zone' do
-    subject = described_class.new("Kings Cross", 1)
-    expect(subject).to have_attributes(:name => "Kings Cross", :zone => 1)
-  end
-end
